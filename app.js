40f5c1b80deeff71fbf9eb253f30d33abd696c8c
ayeshabilalkhan <ayeshabilalkhan7@gmail.com>
2024-07-12 09:34:20 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// JSON Syntax
+var json = {
+    "name": "Arham",
+    "age": 20,
+    "gender": "male",
+    "city": "karachi"
+};
+// Example :
+// JSON object
+var detail = { "name": "Manahil", "age": 19 };
+// accessing JSON object
+console.log(detail.name); // Manahil
+console.log(detail.age); //19
+// Example :
+// JSON object
+var newDetail = {
+    "name": "Ali",
+    "age": 21
+};
+console.log(newDetail.name);
---
+++ app.ts
@@ -0,0 +1,36 @@
+// JSON Syntax
+
+let json = {
+    "name": "Arham",
+    "age": 20,
+    "gender": "male",
+    "city": "karachi"
+}
+
+
+// Example :
+  // JSON object
+
+  const detail = { "name": "Manahil", "age": 19 }
+
+  // accessing JSON object
+ 
+  console.log(detail. name); // Manahil
+  console.log(detail.age); //19
+  
+
+// Example :
+ // JSON object
+
+ const newDetail = {
+
+    "name": "Ali",
+    "age": 21
+}
+console.log(newDetail.name); //Ali
+
+
+// Example :
+ // JSON array
+
+ [ "Rimsha", "Aqsa", "Mahnoor"]
